22dd48fab0b16874976a163976419e84b05ce5da
divyesh sarvaiya <divyeshsarvaiya999@gmail.com>
2023-12-29 11:11:42 +0530

    abc

--- js/style-switcher.js
+++ js/style-switcher.js
@@ -12,19 +12,6 @@ window.addEventListener("scroll", () => {
 })
 
 
-/*        theme colors */
-
-const alternateStyles = document.querySelectorAll(".alternate-style");
-function setActiveStyle(color) {
-    alternateStyles.forEach((style) => {
-        if (color === style.getAttribute("title")) {
-            style.removeAttribute("disabled");
-        }
-        else {
-            style.setAttribute("disabled", "true");
-        }
-    })
-}
 
 
 /* theme light and dark mode */
